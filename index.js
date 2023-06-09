6bd58a4ee274a824c075d8a3a58bd460941000e1
Vasco Ramos <vasco.ramos@ydata.ai>
2023-06-09 14:52:32 +0100

    feat: add action code

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-const core = require('@actions/core');
-const wait = require('./wait');
-
+const github = require("@actions/github");
+const core = require("@actions/core");
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const token = core.getInput("github_token");
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    const octokit = github.getOctokit(token);
+    const context = github.context;
 
-    core.setOutput('time', new Date().toTimeString());
+    const { owner, repo } = context.repo;
+    const sha = core.getInput("sha") || context.sha;
+    const tag = core.getInput("tag", { required: true });
+    const message = core.getInput("message", { required: true });
+    const taggerName = core.getInput("tagger_name");
+    const taggerEmail = core.getInput("tagger_email");
+    const tagger = taggerName && taggerEmail ? { name: taggerName, email: taggerEmail } : undefined;
+
+    const { data: tagResponse } = await octokit.rest.git.createTag({ owner, repo, tag, message, object: sha, sha, type: "commit", tagger });
+    core.info(JSON.stringify(tagResponse));
+    const refResponse = await octokit.rest.git.createRef({ owner, repo, ref: `refs/tags/${tagResponse.tag}`, sha: tagResponse.sha });
+
+    core.setOutput("ref", refResponse.data.ref);
   } catch (error) {
+    core.info(error);
     core.setFailed(error.message);
   }
 }
